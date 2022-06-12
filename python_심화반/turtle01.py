5ef654720ad181975affe04ea5e8088d88fbf0a2
Sehyun Ryan Park <sehyunthebusiness@gmail.com>
2022-06-12 22:34:42 +0900

    Add files via upload

---
+++ python_심화반/turtle01.py
@@ -0,0 +1,24 @@
+import turtle
+
+siri = turtle.Turtle()
+
+# siri.forward(100)
+# siri.left(45)
+# siri.forward(100)
+# siri.right(90)
+# siri.forward(100)
+
+siri.color('#4263f5', '#ad42f5')
+
+siri.begin_fill()
+siri.forward(100)
+siri.left(90)
+siri.forward(100)
+siri.left(90)
+siri.forward(100)
+siri.left(90)
+siri.forward(100)
+siri.end_fill()
+
+
+turtle.done()

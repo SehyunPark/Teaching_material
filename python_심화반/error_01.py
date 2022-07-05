5ec5996d58be1cfd8cc6f34cd93411b15a00385e
Sehyun Ryan Park <sehyunthebusiness@gmail.com>
2022-07-05 14:39:45 +0900

    Add files via upload

---
+++ python_심화반/error_01.py
@@ -0,0 +1,11 @@
+#구문 오류 - 코드에 문제가 있어서 실행 x (SyntaxError)
+
+# print('running..')
+# print('hello) # SyntaxError: EOL while scanning string literal (EOL; End Of Line)
+
+#예외 - 코드 자체 문법적인 오류가 없지만, 실행과 관련된 문제 발생. 실행되나 죽음
+# - try except로 handling
+
+# print('running..')
+# list_a = [1,2,3]
+# print(list_a[100]) #IndexError: list index out of range
